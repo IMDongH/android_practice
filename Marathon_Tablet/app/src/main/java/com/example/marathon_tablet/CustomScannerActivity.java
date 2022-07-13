8ac31620b9e9791b3562c5605e193c957eb9407f
dong <idh1007@naver.com>
2022-07-13 15:02:23 +0900

    바코드 테스트 중

---
+++ Marathon_Tablet/app/src/main/java/com/example/marathon_tablet/CustomScannerActivity.java
@@ -0,0 +1,74 @@
+package com.example.marathon_tablet;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+public class CustomScannerActivity extends CaptureActivity {
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+    private BackPressCloseHandler backPressCloseHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        backPressCloseHandler = new BackPressCloseHandler(this);
+
+        barcodeScannerView = (DecoratedBarcodeView)findViewById(R.id.zxing_barcode_scanner);
+//        barcodeScannerView.setTorchListener((DecoratedBarcodeView.TorchListener) this);
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.setShowMissingCameraPermissionDialog(false);
+        capture.decode();
+
+
+
+
+    }
+    public void onBackPressed() {
+        backPressCloseHandler.onBackPressed();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
+}
