38e9456837813acf4be91833133a203a1c7377e8
dong <idh1007@naver.com>
2022-07-14 17:03:50 +0900

    UI 구현 및 전화 번호 화면 구현

--- Marathon_Tablet/app/src/main/java/com/example/marathon_tablet/PhoneActivity.java
+++ Marathon_Tablet/app/src/main/java/com/example/marathon_tablet/PhoneActivity.java
@@ -1,10 +1,15 @@
 package com.example.marathon_tablet;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,7 +35,7 @@ public class PhoneActivity extends AppCompatActivity {
         //전화번호 형식으로 데이터 표시
         EditText PhoneNumber = (EditText) findViewById(R.id.PhoneNumber);
         PhoneNumber.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
-
+        PhoneNumber.setImeOptions(EditorInfo.IME_ACTION_DONE);
         //상단바 숨김
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
@@ -52,6 +57,28 @@ public class PhoneActivity extends AppCompatActivity {
                 }
             }
         });
+        PhoneNumber.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+            {       if(actionId == EditorInfo.IME_ACTION_DONE)
+                    {
+                        if(validPhoneNumber(PhoneNumber.getText().toString())) {
+                            Intent intent = new Intent(getApplicationContext(), PrintActivity.class);
+                            intent.putExtra("data", PhoneNumber.getText().toString());
+                            intent.putExtra("type", "phone");
+                            startActivity(intent);
+                            return true;
+                        }
+                        else
+                        {
+                            StartToast("올바르지 않은 전화번호 형식입니다.");
+                        }
+                    }
+                    return false;
+                }        });
+
+
 
     }
     @Override
@@ -74,6 +101,18 @@ public class PhoneActivity extends AppCompatActivity {
     private void StartToast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
-
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
 
 }
